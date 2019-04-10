d168ba54ed80a92e123f0c6924cda8023c94171c
Gupta <amrita.gupta@capgemini.com>
2019-04-10 15:45:07 +0530

    chnage webhook url

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.listen(port, "0.0.0.0", function () {
 });
 
 Slack = require('node-slackr');
-slack = new Slack('https://hooks.slack.com/services/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');
+slack = new Slack('https://hooks.slack.com/services/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');
 app.get('/getimage', (req, res) => {
     messages = {
         text: "Display Image",
